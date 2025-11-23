430a456cc4d7281a8427d0d50a0530feb0fe28eb
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-11-23 15:10:31 -0300

    chore: add initial project setup

---
+++ src/main/java/br/db/ecotrack/ecotrack_api/EcotrackApiApplication.java
@@ -0,0 +1,13 @@
+package br.db.ecotrack.ecotrack_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcotrackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcotrackApiApplication.class, args);
+	}
+
+}
