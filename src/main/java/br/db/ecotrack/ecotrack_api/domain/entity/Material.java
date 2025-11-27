21ad77065683eb4da54a6c5868def61d6157c1d3
Denise Mignoli <denisemignoli@gmail.com>
2025-11-27 10:44:53 -0300

    adjust generation type

--- src/main/java/br/db/ecotrack/ecotrack_api/domain/entity/Material.java
+++ src/main/java/br/db/ecotrack/ecotrack_api/domain/entity/Material.java
@@ -20,7 +20,7 @@ import lombok.Setter;
 public class Material {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     @Column(nullable = false, unique = true, length = 100)
