4e81d4232d342accb71239f250f370bc50e21d65
Denise Mignoli <denisemignoli@gmail.com>
2025-11-28 11:36:38 -0300

    remove blank line

--- src/main/java/br/db/ecotrack/ecotrack_api/config/DataInitializer.java
+++ src/main/java/br/db/ecotrack/ecotrack_api/config/DataInitializer.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.Arrays;
 import java.util.List;
 
