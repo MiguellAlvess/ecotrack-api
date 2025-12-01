44317aece84fd5815a321f2a901900fb517baea2
JoaoVSMonteiro <joao_dj58@hotmail.com>
2025-12-01 15:47:50 -0300

    refactor: ajusted method create in disposal

--- src/main/java/br/db/ecotrack/ecotrack_api/service/DisposalService.java
+++ src/main/java/br/db/ecotrack/ecotrack_api/service/DisposalService.java
@@ -1,12 +1,20 @@
 package br.db.ecotrack.ecotrack_api.service;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 
 import br.db.ecotrack.ecotrack_api.controller.request.DisposalRequestDto;
 import br.db.ecotrack.ecotrack_api.controller.response.DisposalResponseDto;
 import br.db.ecotrack.ecotrack_api.domain.entity.Disposal;
+import br.db.ecotrack.ecotrack_api.domain.entity.Material;
+import br.db.ecotrack.ecotrack_api.domain.entity.User;
 import br.db.ecotrack.ecotrack_api.mapper.DisposalMapper;
 import br.db.ecotrack.ecotrack_api.repository.DisposalRepository;
+import br.db.ecotrack.ecotrack_api.repository.MaterialRepository;
+import br.db.ecotrack.ecotrack_api.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 
 @Service
@@ -14,17 +22,35 @@ public class DisposalService {
 
   private final DisposalRepository disposalRepository;
   private final DisposalMapper disposalMapper;
+  private final MaterialRepository materialRepository;
+  private final UserRepository userRepository;
 
-  public DisposalService(DisposalRepository disposalRepository, DisposalMapper disposalMapper) {
+  public DisposalService(DisposalRepository disposalRepository, DisposalMapper disposalMapper,
+      MaterialRepository materialRepository, UserRepository userRepository) {
     this.disposalRepository = disposalRepository;
     this.disposalMapper = disposalMapper;
+    this.materialRepository = materialRepository;
+    this.userRepository = userRepository;
   }
 
   @Transactional
   public DisposalResponseDto createDisposal(DisposalRequestDto disposalRequestDto) {
-    Disposal disposalToSave = disposalMapper.toEntity(disposalRequestDto);
-    Disposal savedDisposal = disposalRepository.save(disposalToSave);
-    return disposalMapper.toDto(savedDisposal);
+    Material material = materialRepository.findById(disposalRequestDto.materialId())
+        .orElseThrow(() -> new EntityNotFoundException("Material not found"));
+
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    Jwt jwt = (Jwt) auth.getPrincipal();
+    String email = jwt.getClaim("email");
+    User user = userRepository.findByEmail(email)
+        .orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado"));
+
+    Disposal disposal = disposalMapper.toEntity(disposalRequestDto);
+
+    disposal.setMaterial(material);
+    disposal.setUser(user);
+    Disposal disposalSaved = disposalRepository.save(disposal);
+
+    return disposalMapper.toDto(disposalSaved);
   }
 
 }
