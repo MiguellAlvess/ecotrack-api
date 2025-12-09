9f6420c1c38ec7023dccfbaf65af57c88cda84eb
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-12-09 17:33:23 -0300

    test: ensure that user repository returns an error if the user data is invalid

--- src/test/java/br/db/ecotrack/ecotrack_api/repository/UserRepositoryTest.java
+++ src/test/java/br/db/ecotrack/ecotrack_api/repository/UserRepositoryTest.java
@@ -1,11 +1,13 @@
 package br.db.ecotrack.ecotrack_api.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
+import static br.db.ecotrack.ecotrack_api.common.UserConstants.INVALID_USER;
 import static br.db.ecotrack.ecotrack_api.common.UserConstants.USER;
 import br.db.ecotrack.ecotrack_api.domain.entity.User;
 
@@ -27,5 +29,13 @@ public class UserRepositoryTest {
     assertThat(sut).isNotNull();
     assertThat(sut.getName()).isEqualTo(USER.getName());
     assertThat(sut.getEmail()).isEqualTo(USER.getEmail());
+  }
+
+  @Test
+  public void createuser_WithInvalidData_ThrowsException() {
+    assertThatThrownBy(() -> {
+      userRepository.save(INVALID_USER);
+      userRepository.flush();
+    });
   }
 }
