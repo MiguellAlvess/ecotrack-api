300963fa09da285f9d3b93a5449350484e84c23b
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-12-09 17:57:30 -0300

    test: ensure that the user repository throws an error when creating a user if the email address is already registered

--- src/test/java/br/db/ecotrack/ecotrack_api/repository/UserRepositoryTest.java
+++ src/test/java/br/db/ecotrack/ecotrack_api/repository/UserRepositoryTest.java
@@ -6,10 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.dao.DataIntegrityViolationException;
 
-import static br.db.ecotrack.ecotrack_api.common.UserConstants.INVALID_USER;
-import static br.db.ecotrack.ecotrack_api.common.UserConstants.USER;
+import static br.db.ecotrack.ecotrack_api.common.UserConstants.invalidUser;
+import static br.db.ecotrack.ecotrack_api.common.UserConstants.validUser;
 import br.db.ecotrack.ecotrack_api.domain.entity.User;
+import jakarta.validation.ConstraintViolationException;
 
 @DataJpaTest
 public class UserRepositoryTest {
@@ -22,20 +24,43 @@ public class UserRepositoryTest {
 
   @Test
   public void createUser_WithValidData_ReturnsUser() {
-    User user = userRepository.save(USER);
+    User request = validUser();
+
+    User user = userRepository.save(request);
 
     User sut = testEntityManager.find(User.class, user.getUserId());
-
     assertThat(sut).isNotNull();
-    assertThat(sut.getName()).isEqualTo(USER.getName());
-    assertThat(sut.getEmail()).isEqualTo(USER.getEmail());
+    assertThat(sut.getName()).isEqualTo(request.getName());
+    assertThat(sut.getEmail()).isEqualTo(request.getEmail());
   }
 
   @Test
-  public void createuser_WithInvalidData_ThrowsException() {
+  public void createUser_WithInvalidData_ThrowsException() {
+    User invalid = invalidUser();
+
     assertThatThrownBy(() -> {
-      userRepository.save(INVALID_USER);
+      userRepository.save(invalid);
       userRepository.flush();
-    });
+    }).isInstanceOf(ConstraintViolationException.class);
+  }
+
+  @Test
+  public void createUser_WithExistingEmail_ThrowsException() {
+    User firstUser = new User();
+    firstUser.setName("First User");
+    firstUser.setEmail("duplicated@email.com");
+    firstUser.setPassword("password123");
+
+    userRepository.saveAndFlush(firstUser);
+
+    User secondUser = new User();
+    secondUser.setName("Second User");
+    secondUser.setEmail("duplicated@email.com");
+    secondUser.setPassword("otherPass123");
+
+    assertThatThrownBy(() -> {
+      userRepository.save(secondUser);
+      userRepository.flush();
+    }).isInstanceOf(DataIntegrityViolationException.class);
   }
 }
