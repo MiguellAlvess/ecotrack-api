4b19cb514b972403a7a1b21b9a4c6056f1213c4c
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-12-09 17:50:50 -0300

    test: fix detached entity issue by generating fresh user instances

--- src/test/java/br/db/ecotrack/ecotrack_api/common/UserConstants.java
+++ src/test/java/br/db/ecotrack/ecotrack_api/common/UserConstants.java
@@ -5,19 +5,24 @@ import java.util.ArrayList;
 import br.db.ecotrack.ecotrack_api.domain.entity.User;
 
 public class UserConstants {
-  public static final User USER = new User(
-      null,
-      "Teste",
-      "test@gmail.com",
-      "password123456",
-      new ArrayList<>(),
-      new ArrayList<>());
 
-  public static final User INVALID_USER = new User(
-      null,
-      "",
-      "",
-      "",
-      new ArrayList<>(),
-      new ArrayList<>());
+  public static User validUser() {
+    return new User(
+        null,
+        "Teste",
+        "test@gmail.com",
+        "password123",
+        new ArrayList<>(),
+        new ArrayList<>());
+  }
+
+  public static User invalidUser() {
+    return new User(
+        null,
+        "",
+        "",
+        "",
+        new ArrayList<>(),
+        new ArrayList<>());
+  }
 }
